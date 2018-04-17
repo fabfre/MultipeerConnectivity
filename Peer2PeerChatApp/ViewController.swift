13106de9e20e73ab50cec007c15dd1b96ef741cf
Fabian Frey <fabian@appdafuer.com>
2018-04-17 13:31:28 +0200

    Initial Commit

---
+++ Peer2PeerChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peer2PeerChatApp
+//
+//  Created by Fabian Frey on 17.04.18.
+//  Copyright © 2018 Fabian Frey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
