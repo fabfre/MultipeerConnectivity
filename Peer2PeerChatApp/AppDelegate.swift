efe49b0d7347151d3c84c64bb86bb989b6565939
Fabian Frey <fabian@appdafuer.com>
2018-05-23 11:39:55 +0200

    deleted signing

--- Peer2PeerChatApp/AppDelegate.swift
+++ Peer2PeerChatApp/AppDelegate.swift
@@ -19,7 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         mpcManager = MPCManager()
-        
         return true
     }
 
